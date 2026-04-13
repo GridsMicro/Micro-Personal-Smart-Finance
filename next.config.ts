efc7a1f31bf1a6ac27451dc3a3f1266121489ec3
GridsMicro <Grids@microtronic.biz>
2026-04-14 03:14:09 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: 'dist',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
